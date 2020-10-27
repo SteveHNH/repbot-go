dd2b5ae5baf285506c6920e4a26c556b97d08110
Stephen Adams <tsadams@gmail.com>
2020-10-27 19:39:12 -0400

    Initial commit
    beginning work on the golang-based repbot
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

---
+++ config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+// Config contains the token for the discord bot
+type Config struct {
+	Token string
+}
+
+// Get reads in the config file and returns a struct
+func Get() *Config {
+	viper.SetConfigFile("config")
+	viper.AddConfigPath(".")
+	viper.SetConfigType("yml")
+	var configuration Config
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("Error reading config file: %s", err)
+	}
+
+	err := viper.Unmarshal(&configuration)
+	if err != nil {
+		fmt.Printf("Unable to decode into struct, %v", err)
+	}
+
+	return configuration
+}
