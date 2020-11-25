619f60106d943aeb07e376a93049b3af7e95002d
Stephen Adams <tsadams@gmail.com>
2020-11-25 09:17:54 -0500

    feat: Add the rest of the working guts
    This gets the repbot up and running. It will initialize users with 1 rep
    and also increment rep. Other features will be incoming.
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- config/config.go
+++ config/config.go
@@ -9,23 +9,27 @@ import (
 // Config contains the token for the discord bot
 type Config struct {
 	Token string
+	DB string
 }
 
 // Get reads in the config file and returns a struct
 func Get() *Config {
 	options := viper.New()
 
-	options.SetConfigFile("config")
+	options.SetConfigFile("botconfig")
+	// TODO: find out why viper seems to only be able to read config from .
+	// For now just put your config wherever you run repbot from
 	options.AddConfigPath(".")
-	options.AddConfigPath("$HOME/.config/repbot-go")
+	options.AddConfigPath("$HOME/.config/repbot-go/")
 	options.AddConfigPath("./config/")
 	options.SetConfigType("yaml")
 
 	if err := options.ReadInConfig(); err != nil {
-		fmt.Printf("Error reading config file: %s", err)
+		fmt.Printf("Error reading config file: %s \n", err)
 	}
 
 	return &Config{
 		Token: options.GetString("token"),
+		DB: options.GetString("db"),
 	}
 }
--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
 	"os/signal"
+	"regexp"
+	"strconv"
 	"syscall"
 
+	config "./config"
 	"github.com/bwmarrin/discordgo"
-	"github.com/stevehnh/repbot-go/config"
+	_ "github.com/mattn/go-sqlite3"
 )
+
+const setRep = `UPDATE reputation SET rep = rep + 1, user = ? WHERE username = ?`
+const getRep = `SELECT rep FROM reputation WHERE username = ?`
+const initRep = `INSERT OR IGNORE into reputation (username, user, rep) values (?, ?, ?)`
 
 func main() {
 	cfg := config.Get()
@@ -37,9 +45,91 @@ func main() {
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	repMsg := `^\!rep\s\<\@\!*\d+\>\s*`
+	//notes for me
+	// mention.username: stephen
+	// mention ID: 123423452345
+	// author ID: 1231242134
+
+	var matched bool
+	//trigger := `^\!rep.*`
+	repAdd := `^\!rep\s\<\@\!*\d+\>\s*`
+	repPing := `^\!rep\sping\s*`
 
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
+	//TODO: Remove after debugging
+	fmt.Println(m.Content)
+	matched, _ = regexp.MatchString(repPing, m.Content)
+	if matched {
+		s.ChannelMessageSend(m.ChannelID, "pong")
+	}
+
+	matched, _ = regexp.MatchString(repAdd, m.Content)
+	if matched {
+		if m.Author.ID != m.Mentions[0].ID {
+			repInc(m.Mentions[0], m, s)
+		} else {
+			s.ChannelMessageSend(m.ChannelID, "you can't update your own rep")
+		}
+	}
 }
+
+func checkUser(u *discordgo.User, db *sql.DB) (result bool, err error) {
+	rows, err := db.Query(getRep, u.ID)
+	if err != nil {
+		fmt.Printf("Couldn't check for user: %s", err)
+	}
+	var repValue int
+	for rows.Next() {
+		rows.Scan(&repValue)
+	}
+	if repValue > 0 {
+		return true, nil
+	}
+	return false, nil
+}
+
+func repInc(u *discordgo.User, m *discordgo.MessageCreate, s *discordgo.Session) {
+	db, _ := sql.Open("sqlite3", config.Get().DB)
+	defer db.Close()
+	usercheck, err := checkUser(u, db)
+	if err != nil {
+		fmt.Printf("Something broke with userCheck: %s", err)
+	}
+	if usercheck == false {
+		statement, err := db.Prepare(initRep)
+		if err != nil {
+			fmt.Printf("Unable to prepare sql statement for initRep: %s", err)
+			s.ChannelMessageSend(m.ChannelID, "Could not initialize new user " + u.Username)
+		}
+		_, err = statement.Exec(u.ID, u.Username, 1)
+		if err != nil {
+			fmt.Printf("initRep execution failed: %s", err)
+			s.ChannelMessageSend(m.ChannelID, "Could not initialize user " + u.Username)
+		} else {
+			s.ChannelMessageSend(m.ChannelID, "Intialized new user with 1 rep! Congrats, " + u.Username)
+		}
+
+	} else {
+		statement, err := db.Prepare(setRep)
+		if err != nil {
+			fmt.Printf("Unable to prepare sql statement for repInc: %s", err)
+			s.ChannelMessageSend(m.ChannelID, "Could not update rep for " + u.Username)
+		}
+		_, err = statement.Exec(u.Username, u.ID)
+		if err != nil {
+			fmt.Printf("repInc execution failed: %s", err)
+			s.ChannelMessageSend(m.ChannelID, "Could not update rep for " + u.Username)
+		} else {
+			rows, _ := db.Query(getRep, u.ID)
+			var repValue int
+			for rows.Next() {
+				rows.Scan(&repValue)
+			}
+			s.ChannelMessageSend(m.ChannelID, "Rep increased to " + strconv.Itoa(repValue) + " for " + u.Username)
+		}
+	}
+}
+
+
